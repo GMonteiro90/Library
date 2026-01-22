80688b92faba5fe0a6158e8aefaf335eb5b952c3
Gonçalo <goncalonsmonteiro@gmail.com>
2026-01-22 19:54:27 +0000

    added buttons for delete and change status on script.js

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
 const myLibrary = [];
 
-function Book(title, author, page, read){
+function Book(title, author, pages, read){
     if (!new.target){
         throw Error("Must use the new operator to call the function");
     }
     this.id = crypto.randomUUID();
     this.title = title;
     this.author = author;
-    this.page = page;
+    this.pages = pages;
     this.read = read;
 }
 
@@ -21,15 +21,39 @@ addBookToLibrary('The Bible', "JC himself", 1000, false);
 addBookToLibrary('Finding Nemo', "IDK", 1, true);
 
 function renderLibrary(){
-    const main = document.querySelector("body");
+    const main = document.querySelector(".display");
+    main.innerHTML = "";
     for (const book of myLibrary){
         const container = document.createElement("div");
+
+        const title = document.createElement("h3");
+        title.textContent = `${book.title} by ${book.author}`;
+        const pages = document.createElement("p");
+        pages.textContent = `Pages: ${book.pages}`;
+        const status = document.createElement("p");
+        status.textContent = `Status: ${book.read ? "Read" : "Not read"}`;
+
+        const deleteButton = document.createElement("button");
+        const readButton = document.createElement("button");
+
         container.classList.add("book");
-        container.textContent = `  ${book.title} by ${book.author}
-                Pages: ${book.page}
-                Status: ${book.read ? "Read" : "Not read"}
-            `;
+        deleteButton.classList.add("delButton")
+        deleteButton.textContent = "Delete"
+        readButton.classList.add("rButton")
+        readButton.textContent = "Change Status"
+        
+        container.append(title, pages, status, readButton, deleteButton);
         main.appendChild(container);
+
+        readButton.addEventListener("click", () => {
+            book.read = !book.read;
+            renderLibrary();
+})
+        deleteButton.addEventListener("click", () => {
+            const index = myLibrary.findIndex((currentBook) => currentBook.id === book.id);
+            myLibrary.splice(index, 1);
+            renderLibrary();
+        });
 }
 };
 renderLibrary();
