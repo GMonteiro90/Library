d947c1596c83a2cac98e925c2f91a78b1ab0b474
Gonçalo <goncalonsmonteiro@gmail.com>
2026-01-22 16:36:23 +0000

    updated script.js to create book, add book and render book

--- script.js
+++ script.js
@@ -1,20 +1,35 @@
 const myLibrary = [];
 
-// book constructor
-
-function Book(title, author, pages, read) {
+function Book(title, author, page, read){
+    if (!new.target){
+        throw Error("Must use the new operator to call the function");
+    }
     this.id = crypto.randomUUID();
     this.title = title;
     this.author = author;
-    this.pages = pages;
+    this.page = page;
     this.read = read;
-};
+}
 
 function addBookToLibrary(title, author, pages, read){
-    const newBook = new Book(title, author, pages, read);
-    myLibrary.push(newBook);
-};
+    const book = new Book(title, author, pages, read);
+    myLibrary.push(book);
+}
 
-addBookToLibrary("The Hobbit", "J.R.R. Tolkien", 295, true);
-addBookToLibrary("1984", "George Orwell", 328, false);
-addBookToLibrary("Clean Code", "Robert C. Martin", 464, true);
+addBookToLibrary('The Shinning', "Stepheng King", 192, true);
+addBookToLibrary('The Bible', "JC himself", 1000, false);
+addBookToLibrary('Finding Nemo', "IDK", 1, true);
+
+function renderLibrary(){
+    const main = document.querySelector("body");
+    for (const book of myLibrary){
+        const container = document.createElement("div");
+        container.classList.add("book");
+        container.textContent = `  ${book.title} by ${book.author}
+                Pages: ${book.page}
+                Status: ${book.read ? "Read" : "Not read"}
+            `;
+        main.appendChild(container);
+}
+};
+renderLibrary();
